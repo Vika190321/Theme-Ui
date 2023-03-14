1257df3088526f54f99ef73327469ba10b195906
Vika <vikina050@gmail.com>
2023-03-14 17:54:14 +0100

    Initial commit

---
+++ react-theme-ui/src/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+    initialColorModeName:'white',
+    colors:  {
+        text: "black",
+        background: "white",
+        primary: "#01408e",
+        inverseText: "white",
+        modes:{
+            pink:{
+                text: "white",
+                background: "pink",
+                primary: "#282c34",
+                inverseText: "8fceff",
+            },
+        },
+    },
+};
+
+export default theme;
